037ee781c5641679ddaddc1b8a9cd11203c3d988
Sergey Avtonomov <avtonomovsergei@yandex.ru>
2024-01-02 19:42:35 +0300

    add input in terminal

--- Program.cs
+++ Program.cs
@@ -4,10 +4,22 @@
     {
         public static void Main()
         {
-            string[] arrayTask = { "Sort", "Arr", "C#" };
+            //string[] arrayTask = { "Sort", "Arr", "C#" };
+            Console.Clear();
+            string stringLoad = "Введите размер массива строк (от 1 до 100) => ";
+            System.Console.Write(stringLoad);
+            int sizeArray = NumberInTerminal(100,stringLoad,1);
+            string[] arrayTask = new string[sizeArray];
+            for (int i = 0; i < arrayTask.Length; i++)
+            {
+                System.Console.WriteLine($"Введите значение для строки №{i}");
+                string ? numString = Console.ReadLine();
+                arrayTask[i] = numString;
+            }
             Console.WriteLine($"Заданный массив => [{String.Join(", ", arrayTask)}]");
             Console.WriteLine($"Отсортированный массив => [{String.Join(", ", SortArray(arrayTask,3))}]");
         }
+
         /// <Сортировка массива строк по количеству симвлов в строке >
         /// 
         /// </summary>
@@ -30,6 +42,22 @@
             }
             Array.Resize(ref arrayResult,j);
             return arrayResult;
+        }
+        /*Функция ввода чисел в терминале*/
+        public static int NumberInTerminal(int numberDigits, string repeatString, int minValueSet)
+        {
+            string ? numString = Console.ReadLine();
+            int numInt = 0;
+            while ((!Int32.TryParse(numString,out numInt)) 
+                    || !(numInt >= minValueSet) 
+                    || !(numInt <= numberDigits)
+                  )
+            {
+                System.Console.WriteLine("Ошибка ввода, повторите");
+                System.Console.Write(repeatString);
+                numString = Console.ReadLine(); 
+            }
+            return numInt;
         }
     }
 }
